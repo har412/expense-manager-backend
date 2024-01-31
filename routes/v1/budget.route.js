4cb7b44a327bae64dbece6bd1ad85efc9ddde172
Harkirat Singh <hs3188248@gmail.com>
2024-02-01 01:36:29 +0530

    fixes in budget and charity

--- routes/v1/budget.route.js
+++ routes/v1/budget.route.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const { verifyUser } = require('../../middlewares/auth.verify')
-const { insertBudget } = require('../../services/budget.service')
-const { getAllBudgets, editBudget, removeBudget } = require('../../controllers/budget.controller')
+const { getAllBudgets, editBudget, removeBudget, addBudget } = require('../../controllers/budget.controller')
 
 
 const router = express.Router()
 
-router.route('/add').post(verifyUser , insertBudget)
+router.route('/add').post(verifyUser , addBudget)
 router.route('/').get(verifyUser , getAllBudgets)
 router.route('/update/:id').post(verifyUser , editBudget)
 router.route('/delete/:id').delete(verifyUser , removeBudget)
--- routes/v1/charity.route.js
+++ routes/v1/charity.route.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const { verifyUser } = require('../../middlewares/auth.verify')
-const { insertCharity } = require('../../services/charity.service')
-const { getAllCharitys, editCharity, removeCharity } = require('../../controllers/charity.controller')
+const { getAllCharitys, editCharity, removeCharity, addCharity } = require('../../controllers/charity.controller')
 
 
 const router = express.Router()
 
-router.route('/add').post(verifyUser , insertCharity)
+router.route('/add').post(verifyUser , addCharity)
 router.route('/').get(verifyUser , getAllCharitys)
 router.route('/update/:id').post(verifyUser , editCharity)
 router.route('/delete/:id').delete(verifyUser , removeCharity)
