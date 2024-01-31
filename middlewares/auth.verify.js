96a54a045946174aa2d6faadc3ea40c02dca4bcc
Harkirat Singh <hs3188248@gmail.com>
2024-01-31 22:56:24 +0530

    added verify user middleware

---
+++ middlewares/auth.verify.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+const { handleResponse } = require('../utils/responseHandler')
+const httpStatus = require('http-status')
+
+
+const verifyUser = async (req,res,next) =>{
+    
+    try {
+        let token = req.header('Authorization');
+        token = token.split(' ')[1];
+        const user = await jwt.verify(token,process.env.SECRET)
+        next()
+    } catch (error) {
+        handleResponse(
+            res,
+            httpStatus.UNAUTHORIZED,
+            error.message,
+            'UNAUTHORIZED'
+        )
+    }
+
+}
+
+
+module.exports = {verifyUser}
--- routes/v1/expense.route.js
+++ routes/v1/expense.route.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const { addExpense, getAllExpenses, editExpense, removeExpense } = require('../../controllers/expense.controller')
+const { verifyUser } = require('../../middlewares/auth.verify')
 
 const router = express.Router()
 
-router.route('/add').post(addExpense)
-router.route('/').get(getAllExpenses)
-router.route('/update/:id').post(editExpense)
-router.route('/delete/:id').delete(removeExpense)
+router.route('/add').post(verifyUser , addExpense)
+router.route('/').get( verifyUser , getAllExpenses)
+router.route('/update/:id').post( verifyUser , editExpense)
+router.route('/delete/:id').delete( verifyUser , removeExpense)
 
 module.exports = router
